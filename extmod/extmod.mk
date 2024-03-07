remenyo/micropython

extmod/vfs_map: Add VfsMap filesystem object.
Signed-off-by: Damien George <damien@micropython.org>

extmod/extmod.mk
@@ -41,6 +41,8 @@ PY_EXTMOD_O_BASENAME = \
 	extmod/vfs_fat_diskio.o \
 	extmod/vfs_fat_file.o \
 	extmod/vfs_lfs.o \
+	extmod/vfs_map.o \
+	extmod/vfs_map_file.o \
 	extmod/utime_mphal.o \
 	extmod/uos_dupterm.o \
 	shared/libc/abort_.o \

thbtech/mpython

extmod/extmod.mk
@@ -55,6 +55,8 @@ SRC_EXTMOD_C += \
 	extmod/vfs_fat_diskio.c \
 	extmod/vfs_fat_file.c \
 	extmod/vfs_lfs.c \
+	extmod/vfs_map.c \
+	extmod/vfs_map_file.c \
 	extmod/vfs_posix.c \
 	extmod/vfs_posix_file.c \
 	extmod/vfs_reader.c \