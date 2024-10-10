57472a3f2dbb3bf60767d7e2cd19c82e5f718893
Peter Harper <peter.harper@raspberrypi.com>
2024-10-10 19:35:57 +0100

    rp2: Allow pico to use cyw43.

--- ports/rp2/boards/RPI_PICO2/mpconfigboard.h
+++ ports/rp2/boards/RPI_PICO2/mpconfigboard.h
@@ -1,3 +1,9 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico2"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024)
+
+#if MICROPY_PY_NETWORK_CYW43
+#include "enable_cyw43.h"
+#define CYW43_PIN_WL_DYNAMIC 1
+#define CYW43_PIO_CLOCK_DIV_DYNAMIC 1
+#endif
